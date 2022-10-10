c5c90c447c25e90dd873396fced686952cf12530
vinayakkurugovinakoppa <vinayakkurugovinakoppa@qnap.com>
2022-10-10 22:18:16 +0530

    media queries added for different devices

--- src/Components/navigation/index.jsx
+++ src/Components/navigation/index.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import logo from '../../assets/logo.svg'
 import './style.scss'
 
+const Menu = () => (
+  <>
+    <p><a href="#home">Home</a></p>
+    <p><a href="#wgpt3">What is GPT?</a></p>
+    <p><a href="#possibility">Open AI</a></p>
+    <p><a href="#features">Case Studies</a></p>
+    <p><a href="#blog">Library</a></p>
+  </>
+)
+
 const Navigation = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
     <div className="navbar">
       <div className="navbar-links">
@@ -11,15 +22,29 @@ const Navigation = () => {
           <img src={logo} alt="Logo" />
         </div>
         <div className="navbar-links__container">
-          <p><a href="#home">Home</a></p>
-          <p><a href="#wgpt3">What is GPT?</a></p>
-          <p><a href="#possibility">Open AI</a></p>
-          <p><a href="#features">Case Studies</a></p>
-          <p><a href="#blog">Library</a></p>
+          <Menu />
         </div>
         <div className="navbar-sign">
           <p>Sign in</p>
           <button type="button">Sign up</button>
+        </div>
+        <div className="navbar-menu">
+          {toggleMenu ? (
+            <RiCloseLine color='#fff' size={27} onClick={() => setToggleMenu(false)} />
+          ) : (
+            <RiMenu3Line color='#fff' size={27} onClick={() => setToggleMenu(true)} />
+          )}
+          {toggleMenu && (
+            <div className="navbar-menu__container scale-up-center">
+              <div className="navbar-menu__container-links">
+                <Menu />
+              </div>
+              <div className="navbar-menu__container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
